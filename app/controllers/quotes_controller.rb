f48dfdfe98a6683acee181a5d2b4cbd0e230ff73
keegs <kwincewicz@mba2015.hbs.edu>
2014-10-04 18:51:53 +0000

    we added the simple form and then we did the launch button and it looks like it is working

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,7 +1,25 @@
 class QuotesController < ApplicationController
-def index
+	def index
 	@quote = Quote.order("RANDOM()").last
+	
+	
+	end
 
-end
+	def new
+		@quote = Quote.new
+
+
+	end
+
+	def create
+		Quote.create(quote_params)
+		redirect_to quotes_path
+	end
+
+	private
+
+	def quote_params
+		params.require(:quote).permit(:saying, :author)
+	end
 
 end
